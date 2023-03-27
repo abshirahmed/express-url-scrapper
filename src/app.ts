ce0de9013987b9ec2625197dc8755067fd932860
Abshir <abshir.y.ahmed@gmail.com>
2023-03-27 01:49:40 +0100

    update app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-import helloWorld from './routes/helloWorld';
+import crawl from './routes/crawl';
+import { json } from 'body-parser';
 
 export const app = express();
 
 // middleware
 app.use(cors());
+app.use(json());
 
 // routes
-app.use('/', helloWorld);
+app.use('/crawl', crawl);
